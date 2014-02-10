ccc63f609cb0398aa812fc433dcf9a5761843936
Josue <Josue@10.230.168.110>
2014-02-10 14:03:49 +0100

    Added singleOperatorExpression test and passed

--- test/expressionevaluator/ExpressionEvaluatorTest.java
+++ test/expressionevaluator/ExpressionEvaluatorTest.java
@@ -4,28 +4,48 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class ExpressionEvaluatorTest {
-    
+
     @Test
     public void constantExpressionTest() {
         Expression expression = new Expression(1);
         Assert.assertEquals(1, expression.calculate());
     }
-    
+
     @Test
     public void otherConstantExpressionTest() {
         Expression expression = new Expression(2);
         Assert.assertEquals(2, expression.calculate());
     }
 
+    @Test
+    public void singleOperandExpressionTest() {
+        Expression expression = new Expression("+", new Expression(2), new Expression(3));
+        Assert.assertEquals(5, expression.calculate());
+    }
+
     public class Expression {
-        private int value;
-        
+
+        private String value;
+        private boolean isOperator;
+        private Expression rightExpression, leftExpression;
+
         public Expression(int value) {
-            this.value = value;
+            this.value = String.valueOf(value);
+            this.isOperator = false;
         }
-        
+
+        public Expression(String operand, Expression rightExpression, Expression leftExpression) {
+            this.value = operand;
+            this.isOperator = true;
+            this.rightExpression = rightExpression;
+            this.leftExpression = leftExpression;
+        }
+
         public int calculate() {
-            return value;
+            if (isOperator) {
+                return rightExpression.calculate() +  leftExpression.calculate();
+            } 
+            else return Integer.valueOf(value);
         }
     }
 }
