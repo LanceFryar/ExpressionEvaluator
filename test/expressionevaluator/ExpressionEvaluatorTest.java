6166b4542da4d99d8aef0b31d3115225aac94ba9
Josue <Josue@10.230.168.110>
2014-02-10 13:18:03 +0100

    Deleted otherTest

--- test/expressionevaluator/ExpressionEvaluatorTest.java
+++ test/expressionevaluator/ExpressionEvaluatorTest.java
@@ -11,12 +11,6 @@ public class ExpressionEvaluatorTest {
         Assert.assertEquals(7, expressionEvaluator.evaluateExpression("1+2*3"));
     }
     
-    @Test
-    public void otherTest() {
-        ExpressionEvaluator expressionEvaluator = new ExpressionEvaluator();
-        Assert.assertEquals(9, expressionEvaluator.evaluateExpression("1+2*3+2"));
-    }
-
     public class ExpressionEvaluator {
 
         public ExpressionEvaluator() {
