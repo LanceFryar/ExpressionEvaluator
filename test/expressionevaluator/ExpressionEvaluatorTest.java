d71b77338832220bcd00c6b0adfb7ae731019e0d
Rayco <Rayco@10.230.174.112>
2014-02-10 14:23:38 +0000

    BinaryOperation refactored

--- test/expressionevaluator/ExpressionEvaluatorTest.java
+++ test/expressionevaluator/ExpressionEvaluatorTest.java
@@ -17,7 +17,7 @@ public class ExpressionEvaluatorTest {
 
     @Test
     public void singleOperandExpressionTest() {
-        BinaryOperation binaryOperation = new BinaryOperation("+", new Constant(2), new Constant(3));
+        BinaryOperation binaryOperation = new BinaryOperation(new Constant(2), new Constant(3));
         Assert.assertEquals(5, binaryOperation.calculate());
     }
 
@@ -43,7 +43,7 @@ public class ExpressionEvaluatorTest {
     public class BinaryOperation implements Expression {
         private final Expression rightExpression, leftExpression;
 
-        public BinaryOperation(String operand, Expression leftExpression, Expression rightExpression) {
+        public BinaryOperation(Expression leftExpression, Expression rightExpression) {
             this.rightExpression = rightExpression;
             this.leftExpression = leftExpression;
         }
