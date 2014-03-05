5242be1270c26371fd51abb4bbb598e7fe8e5970
Rayco <Rayco@10.230.169.73>
2014-03-05 09:10:10 +0000

    ParserTest created

---
+++ test/expressionevaluator/parser/Token.java
@@ -0,0 +1,9 @@
+package expressionevaluator.parser;
+
+public class Token {
+
+    Token(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
